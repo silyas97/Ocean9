bcd0882508415dc54dc477948c185d9ca1a2d97b
sobia <sobia@10.67.225.27>
2021-12-25 17:48:04 -0500

    First commit

---
+++ src/test/java/Pages/GoogleHomepage.java
@@ -0,0 +1,18 @@
+package Pages;
+
+public class GoogleHomepage {
+
+ WebDriver driver; 	
+	
+Google Homepage (Webdriver dr) {
+	driver=dr; 
+	
+	@Findby(name="q")
+	WebElement searchBox;
+	
+	public void enterSearchTerm(String term) {
+		Search
+	}
+}
+	
+}
